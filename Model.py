140d2bc5cabab3ebdd6efcdd5f808aabdf2c0f45
PetrashU <102193722+PetrashU@users.noreply.github.com>
2023-11-28 01:32:12 +0100

    Update Model.py

--- Model.py
+++ Model.py
@@ -5,6 +5,7 @@ config = ConfigParser()
 config.read('db.ini')
 db = MySQLDatabase(config['DataBase']['name'], user=config['DataBase']['user'], password=config['DataBase']['pass'], host=config['DataBase']['host'], port=config.getint('DataBase','port'))
 
+
 class Shirt(Model):
     id = AutoField(primary_key=True)
     color = CharField()
